ce85a10b22a26802f4ea7d39c6a7f2f723741be2
itstherall <14thereza@gmail.com>
2025-06-07 00:39:17 -0300

    corrigindo o message sucess para não mostrar  a mensagem quando tentar submeter um erro

--- main.js
+++ main.js
@@ -13,32 +13,34 @@ form.addEventListener('submit', function(e){
 
     const mensagemSucess = 'Tudo certo! Podemos prosseguir.';
     const mensagemError = 'O numero B precisa ser maior que o A!';
+    const containerMS = document.querySelector('.message-sucess');
+    const containerME = document.querySelector('.message-error');
     
     formValido = validaFormulario(numA.value, numB.value);
     if(formValido){
-        const containerMS = document.querySelector('.message-sucess');
         containerMS.innerHTML = mensagemSucess;
         containerMS.style.display = 'block';
 
-        
+        numA.value = '';
+        numB.value = '';  
     } else {
-        const containerME = document.querySelector('.message-error');
+        containerMS.innerHTML = '';
         containerME.innerHTML = mensagemError;
         numB.style.border = '1px solid red';
         containerME.style.display = 'block';
     }
-    
-    numB.addEventListener('keyup', function(e){
+})
+
+numB.addEventListener('keyup', function(e){
         const containerME = document.querySelector('.message-error');
         formValido = validaFormulario(numA.value, e.target.value);
 
         if(!formValido){
             containerME.innerHTML = mensagemError;
+            numB.classList.add('.input-error');
         } else {
             containerME.innerHTML = '';
             numB.style = '';
             containerME.style.display = 'none';
         }
     })
-
-})
